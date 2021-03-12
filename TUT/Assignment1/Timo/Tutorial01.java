d3857f697f5b10e30ffb3f96448732b2900107a9
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-03-12 10:51:57 +0100

    Add javadoc; rename class

---
+++ TUT/Assignment1/Timo/Tutorial01.java
@@ -0,0 +1,9 @@
+package Assignment1.Timo;
+
+/**
+ * Main class for the first tutorial.
+ * Created on 12.03.2021.
+ * @author Timo Tabertshofer
+ */
+public class Tutorial01 {
+}
--- TUT/Assignment1/Timo/Tutorium01.java
+++
@@ -1,4 +0,0 @@
-package Assignment1.Timo;
-
-public class Tutorium01 {
-}
